cae86d3c87d7acd97b0203eb5448f93ef8be8648
SalaheEddine BOUHDJEUR <sbouhdjeur@b114-14.iut.univ-paris8.fr>
2023-01-12 12:45:39 +0100

    Init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Question préliminaire
+
+
+        // Question 1
+
+    }
+
+    public static void triSelection(int[] tab) {
+
+    }
+}
